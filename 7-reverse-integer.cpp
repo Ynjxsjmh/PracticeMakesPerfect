d41a436147d02a5d12a5666985203f400899f6af
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-03-30 23:59:08 +0800

    solved 7

--- 7-reverse-integer.cpp
+++ 7-reverse-integer.cpp
@@ -1,3 +1,15 @@
 int reverse(int x) {
+    long sum = 0;
 
+    while (x) {
+        int rest = x % 10;
+
+        if (sum*10 > 2147483647 || sum*10 < -2147483648) {
+            return 0;
+        }
+        sum = sum*10 + rest;
+        x = x / 10;
+    }
+
+    return sum;
 }
