a0025699e9962212c79b97cc32a607023c637d84
Ynjxsjmh <ynjxsjmh@gmail.com>
2019-07-02 22:20:04 +0800

    [LeetCode] Solve 136

---
+++ LeetCode/136-single-number.cpp
@@ -0,0 +1,11 @@
+// 标签：位运算、哈希表
+
+int singleNumber(vector<int>& nums) {
+    int result = 0;
+
+    for (int i = 0; i < nums.size(); i++) {
+        result ^= nums[i];
+    }
+
+    return result;
+}
