5aed23069d8a765edfa9c9b32e5b9c7c76665ab8
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-04-30 15:55:30 +0800

    [LeetCode] Solve p19

---
+++ LeetCode/p0019/Solution1.cpp
@@ -0,0 +1,50 @@
+/**
+ * Description
+ *
+ *
+ * Example
+ * Input:
+ * Output:
+ * Explanation:
+ *
+ * @author Ynjxsjmh
+ * @email  ynjxsjmh@gmail.com
+ * @since  2020-04-30 15:52
+ * @tag    Two Pointers
+ * @repeat
+ */
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+
+class Solution {
+public:
+    ListNode* removeNthFromEnd(ListNode* head, int n) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* fast = dummy;
+        ListNode* slow = dummy;
+
+        for (int i = 0; i < n; i++) {
+            fast = fast->next;
+        }
+
+        while (fast->next) {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        slow->next = slow->next->next;
+
+        return dummy->next;
+    }
+};
