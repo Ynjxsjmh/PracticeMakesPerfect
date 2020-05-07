20b72d985ee5c38a47a618b1f573c769b87706f8
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-05-07 22:42:11 +0800

    [LeetCode] Solve p139

---
+++ LeetCode/p0139/Solution2.cpp
@@ -0,0 +1,39 @@
+/**
+ * Description
+ *
+ *
+ * Example
+ * Input:
+ * Output:
+ * Explanation:
+ *
+ * @author Ynjxsjmh
+ * @email  ynjxsjmh@gmail.com
+ * @since  2020-05-07 22:40
+ * @tag
+ * @repeat
+ */
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> wordSet(wordDict.begin(), wordDict.end());
+        int n = s.size();
+        vector<bool> dp(n+1, false);
+
+        // Init
+        dp[0] = true;
+
+        // DP
+        for (int i = 0; i < n+1; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && wordSet.count(s.substr(j, i - j))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+
+        return dp[n];
+    }
+};
